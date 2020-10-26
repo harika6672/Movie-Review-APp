0eb3480f1cb27ff82ff0dfa696d540b78416de90
harika6672 <rajasiri1998@gmail.com>
2020-10-26 14:23:47 +0530

    1.0.0

--- client/src/Admin/AdminNavDisplay.js
+++ client/src/Admin/AdminNavDisplay.js
@@ -22,58 +22,58 @@ const AdminNavDisplay = () => {
         </div>
         <div className="collapse navbar-collapse" id="globalNavbarAdmin">
           <ul className="navbar-nav mr-auto order-1">
-            <li className="nav-item">
+            <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
               <Link to="/admin/show-categories" className="nav-link">
                 Show Categories
               </Link>
             </li>
 
-            <li className="nav-item">
+            <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
               <Link to="/admin/show-genres" className="nav-link">
                 Show Genres
               </Link>
             </li>
-            <li className="nav-item">
+            <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
               <Link to="/admin/show-languages" className="nav-link">
                 Show Languages
               </Link>
             </li>
-            <li className="nav-item">
+            <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
               <Link to="/admin/show-users" className="nav-link">
                 Show Users
               </Link>
             </li>
-            <li className="nav-item">
+            <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
               <Link to="/admin/show-posts" className="nav-link">
                 Show Posts
               </Link>
             </li>
-            <li className="nav-item">
+            <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
               <Link to="/admin/show-comments" className="nav-link">
                 Show Comments
               </Link>
             </li>
-            <li className="nav-item">
+            <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
               <Link to="/admin/add-category" className="nav-link">
                 Add Category
               </Link>
             </li>
-            <li className="nav-item">
+            <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
               <Link to="/admin/add-genre" className="nav-link">
                 Add Genre
               </Link>
             </li>
-            <li className="nav-item">
+            <li className="nav-item"  data-toggle="collapse" data-target=".navbar-collapse.show">
               <Link to="/admin/add-language" className="nav-link">
                 Add Language
               </Link>
             </li>
-            <li className="nav-item">
+            <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
               <Link to="/admin/add-post" className="nav-link">
                 Add Movie Review
               </Link>
             </li>
-            <li className="nav-item">
+            <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
               <Link to="/admin/add-user" className="nav-link">
                 Add User
               </Link>
